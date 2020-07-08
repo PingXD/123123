1507b3f51ae35cda77a81f3c1d72147de093fd2b
PingX <admin@pingx.cc>
2020-07-09 01:17:57 +0800

    用于后续对view to model的处理 现在没用，留着先

---
+++ StoreSystem.MVC/Models/VUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Text;
+
+namespace StoreSystem.MVC.Models
+{
+  public class VUser
+  {
+      /// <summary>
+      /// 用户名
+      /// </summary>
+      [Required,StringLength(16)]
+      public string UserName { get; set; }
+      /// <summary>
+      /// 密码
+      /// </summary>
+      [Required, StringLength(32)]
+      public string PassWord { get; set; }
+      /// <summary>
+      /// email
+      /// </summary>
+      [EmailAddress]
+      public string Email { get; set; }
+      /// <summary>
+      /// 手机号
+      /// </summary>
+     [Required,StringLength(11),Phone]
+      public string PhoneNum { get; set; }
+  }
+}
