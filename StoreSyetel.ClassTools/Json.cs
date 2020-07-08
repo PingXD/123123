4adf4ce2833cbb198ae3b03e4fad92475ca111c8
PingX <admin@pingx.cc>
2020-07-09 01:16:38 +0800

    添加了jsontomodels工具类

---
+++ StoreSyetel.ClassTools/Json.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace StoreSystem.ClassTools
+{
+    public static class Json
+    {
+        public static List<T> JsonToModel<T>(string jsonStr)
+        {
+            DataContractJsonSerializer dcjs = new DataContractJsonSerializer(typeof(List<T>));
+            jsonStr = "[" + jsonStr + "]";
+            using (MemoryStream ms = new  MemoryStream(Encoding.UTF8.GetBytes(jsonStr)))
+            {
+              return (List<T>)dcjs.ReadObject(ms);
+            }
+        }
+       
+        
+    }
+}
