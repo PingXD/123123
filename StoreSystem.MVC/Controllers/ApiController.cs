27576ab803ae1486b533c61edb3950f4f47042a6
PingX <admin@pingx.cc>
2020-07-09 01:17:14 +0800

    添加了一个用户注册的post控制器路由

--- StoreSystem.MVC/Controllers/ApiController.cs
+++ StoreSystem.MVC/Controllers/ApiController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StoreSystem.BLL;
 using StoreSystem.MVC.Models;
+using StoreSystem.ClassTools;
+using System.Text;
 
 namespace StoreSystem.MVC.Controllers
 {
@@ -26,5 +29,29 @@ namespace StoreSystem.MVC.Controllers
             
             
         //}
+        [HttpPost]
+        public IActionResult Registered()
+        {
+            try
+            {
+                int dataLen = Convert.ToInt32(Request.Body.Length);
+                byte[] bytes = new byte[dataLen];
+                Request.Body.Read(bytes, 0, dataLen);
+                //var m =ClassTools.Json.JsonToModel<Dto.DtoUser>(Encoding.UTF8.GetString(bytes)).FirstOrDefault();
+                using (IBLL.IUser iu = new User())
+                {
+                    iu.Registered(ClassTools.Json.JsonToModel<Dto.DtoUser>(Encoding.UTF8.GetString(bytes))
+                            .FirstOrDefault());
+                }
+
+                return Ok("\"result\":success");
+            }
+            catch
+            {
+                return NotFound();
+            }
+
+        }
+
     }
 }
